2bf3e0c6fa3d14fa72d0b0cd31726fe9e087e836
lotos580 <vothcicev_k@mail.ru>
2017-03-19 23:34:18 +0300

    Update Exchange.java

--- Exchange.java
+++ Exchange.java
@@ -15,25 +15,24 @@ public class Exchange {
 
     public static void main(String[] args) {
 
+        // Inital prices array
         int priceArray[] = {100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94};
-
         for (Integer iPrice = 0; iPrice < priceArray.length; iPrice++) {
             String s = Integer.toString(priceArray[iPrice]);
             System.out.print(s + "," + " ");
         }
-
-
-        Integer priceDelta[] = new Integer[priceArray.length - 1];
         
+        // Create delta price array
+        Integer priceDelta[] = new Integer[priceArray.length - 1];
         for (int i = 0; i < priceDelta.length; i++) {
             priceDelta[i] = priceArray[i + 1] - priceArray[i];
             String s = Integer.toString(priceDelta[i]);
         }
 
+        int firstDayNumbers = 0, lastDayNumbers = 0, earningNumbers = 0,
+                firstDay = 0, lastDay = 0, earnings = 0;
         
-        int firstDayNumbers = 0, lastDayNumbers = 0, earningNumbers = 0;
-        int firstDay = 0, lastDay = 0, earnings = 0;
-        
+        // 
         for (int i = 0; i < priceDelta.length; ++i) {
             int newDelta = priceDelta[i];
             if (firstDayNumbers == 0) {
@@ -54,8 +53,6 @@ public class Exchange {
                 earnings = earningNumbers;
             }
         }
-
-        
         System.out.print("\nPurchase: day " + Integer.toString(firstDay + 1) + 
                 " for " + Integer.toString(priceArray[firstDay]) +
                 ";\nSale: day " + Integer.toString(lastDay + 1)  +
